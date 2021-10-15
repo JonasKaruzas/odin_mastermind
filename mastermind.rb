2430b3141a0f045383a6ee6602c4602746e57c2c
JonasKaruzas <jonas.karuzas@gmail.com>
2021-10-15 17:56:11 +0300

    initial

---
+++ mastermind.rb
@@ -0,0 +1,45 @@
+# Intro, instructions
+# Maker or braker
+
+# IF breaker
+  # generate code
+
+  # ask for a quess
+  # give result
+  # if not correct REPEAT
+
+# IF Maker
+  # enter code
+
+  # ask for a quess
+  # give result
+  # if not correct REPEAT
+
+class Game
+  def initialize
+
+  end
+
+  def introduction
+
+  end
+
+  def game_mode
+  
+  end
+
+  def generate_code
+  
+  end
+
+  def enter_guess
+  
+  end
+
+  def give_clues
+  
+  end
+
+
+
+end
